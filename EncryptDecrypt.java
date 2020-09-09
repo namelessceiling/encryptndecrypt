afde7fe5428fd9d82afcd50d4ee8ce5e064bccfb
namelessceiling <kelly.k.eng96@gmail.com>
2020-09-09 13:36:12 -0400

    Caesar cipher for ASCII range 0-127

--- EncryptDecrypt.java
+++ EncryptDecrypt.java
@@ -5,17 +5,23 @@ public class EncryptDecrypt {
     
     }
     
-    public static void caesarCipher(String message, int key) {
+    public static void caesarCipherEncrypt(String message, int key) {
         StringBuilder str = new StringBuilder();
         char[] c = message.toCharArray();
         for(int i = 0; i < c.length; i++) {
-            if(Character.isLetter(c[i])) {
-                char newRes = (char) ((c[i] + key - 'a') % 26 + 'a'); //shifts the letter "key" spaces
-                str.append(newRes);
-            } else {
-                str.append(c[i]);
-            }
+            char encrypted = (char) ((c[i] + key) % 128); //Assumes ASCII with 128 chars
+            str.append(encrypted);
         }
         System.out.println(str.toString());
+    }
+    
+    public static void caesarDecrypt(String message, int key) {
+    	StringBuilder str = new StringBuilder();
+    	char[] c = message.toCharArray();
+    	for(int i = 0; i < c.length; i++) {
+    	    char decrepted = (char) Math.abs((c[i] - key) % 128); //ensures it stays in the ASCII 128 chars range of 0-127
+    	    str.append(decrypted);
+    	}
+    	System.out.println(str.toString());
     }
 }
